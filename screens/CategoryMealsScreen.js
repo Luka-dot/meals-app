7ed07b7ba93faf6510634def11998d091f1c0761
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-06-15 10:29:39 -0400

    redux set up in CategoryMeals

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { CATEGORIES, MEALS } from '../data/dummy-data';
+import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
+
+  // retrieving date from state (redux)
+  const availableMeals = useSelector(state => state.meals.filteredMeals);  // meals is set up in app.js inside rootReducer. 
+                                                                          // rootReducer => filteredMeals is inside Reducer/meals.js
+
   //  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-  const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+  const displayedMeals = availableMeals.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
   return (
     <MealList listData={displayedMeals} navigation={props.navigation} />
